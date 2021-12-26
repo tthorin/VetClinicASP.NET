8f42ffd2200ad64edd56e0f465d915d43f3b1dd2
Thomas Thorin <thomas.thorin@gmail.com>
2021-12-26 22:03:52 +0100

    small cleanup

--- VetClinicApp/MongoDbAccess/Database/MongoDbAccess.cs
+++ VetClinicApp/MongoDbAccess/Database/MongoDbAccess.cs
@@ -39,6 +39,11 @@ namespace MongoDbAccess.Database
             var animalCount = await AnimalCollection.EstimatedDocumentCountAsync();
             return ((int)customerCount, (int)animalCount);
         }
+        public async Task<List<Animal>> GetAllAnimal()
+        {
+            var result = await AnimalCollection.FindAsync(_ => true);
+            return result.ToList();
+        }
 
         public async Task<Animal> GetAnimalById(string id)
         {
@@ -53,10 +58,63 @@ namespace MongoDbAccess.Database
             return result.ToList();
         }
 
-        public async Task<List<Animal>> GetAllAnimal()
+
+        public async Task CreateAnimal(Animal animal)
         {
-            var result = await AnimalCollection.FindAsync(_ => true);
-            return result.ToList();
+            animal.Name = Prettify(animal.Name);
+            await AnimalCollection.InsertOneAsync(animal);
+            var ch = Factory.GetCustomerDbHelper();
+            await ch.AddAnimalToCustomer(animal);
+        }
+        public async Task<bool> UpdateAnimal(Animal animal)
+        {
+            animal.Name = Prettify(animal.Name);
+            var filter = Builders<Animal>.Filter.Eq("Id", animal.Id);
+            var result = await AnimalCollection.ReplaceOneAsync(filter, animal, new ReplaceOptions { IsUpsert = true });
+            if (result.IsModifiedCountAvailable && result.ModifiedCount > 0)
+            {
+                var cdbh = Factory.GetCustomerDbHelper();
+                await cdbh.UpdateAnimalOnCustomer(animal);
+                return true;
+            }
+            return false;
+        }
+        public async Task<bool> DeleteAnimal(Animal animal)
+        {
+            var result = await AnimalCollection.DeleteOneAsync(x => x.Id == animal.Id);
+            if (result.IsAcknowledged && result.DeletedCount > 0)
+            {
+                var cdbh = Factory.GetCustomerDbHelper();
+                await cdbh.RemoveAnimalFromCustomer(animal);
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<Customer> GetCustomerById(string id)
+        {
+            var filter = Builders<Customer>.Filter.Eq("Id", id);
+            var output = await CustomerCollection.FindAsync(filter);
+            return output.FirstOrDefault();
+        }
+
+
+
+        public async Task<bool> UpdateCustomer(Customer owner)
+        {
+            owner.FirstName = Prettify(owner.FirstName);
+            owner.LastName = Prettify(owner.LastName);
+            var filter = Builders<Customer>.Filter.Eq("Id", owner.Id);
+            var result = await CustomerCollection.ReplaceOneAsync(filter, owner, new ReplaceOptions { IsUpsert = true });
+            return result.IsModifiedCountAvailable && result.ModifiedCount > 0;
+        }
+
+        public async Task<bool> DeleteCustomerById(string id)
+        {
+            var cdbh = Factory.GetCustomerDbHelper();
+            await cdbh.DeleteAnimalsTogetherWithCustomer(id);
+            var result = await CustomerCollection.DeleteOneAsync(x => x.Id == id);
+            return result.IsAcknowledged && result.DeletedCount > 0;
         }
 
         public Task CreateCustomer(Customer owner)
@@ -78,64 +136,6 @@ namespace MongoDbAccess.Database
                 x.LastName.ToLower().StartsWith(beginsWith.ToLower()) ||
                 x.FirstName.ToLower().StartsWith(beginsWith.ToLower()));
             return result.ToList();
-        }
-
-        public async Task CreateAnimal(Animal animal)
-        {
-            animal.Name = Prettify(animal.Name);
-            await AnimalCollection.InsertOneAsync(animal);
-            var ch = Factory.GetCustomerDbHelper();
-            await ch.AddAnimalToCustomer(animal);
-        }
-
-        public async Task<Customer> GetCustomerById(string id)
-        {
-            var filter = Builders<Customer>.Filter.Eq("Id", id);
-            var output = await CustomerCollection.FindAsync(filter);
-            return output.FirstOrDefault();
-        }
-
-        public async Task<bool> UpdateAnimal(Animal animal)
-        {
-            animal.Name = Prettify(animal.Name);
-            var filter = Builders<Animal>.Filter.Eq("Id", animal.Id);
-            var result = await AnimalCollection.ReplaceOneAsync(filter, animal, new ReplaceOptions { IsUpsert = true });
-            if (result.IsModifiedCountAvailable && result.ModifiedCount > 0)
-            {
-                var cdbh = Factory.GetCustomerDbHelper();
-                await cdbh.UpdateAnimalOnCustomer(animal);
-                return true;
-            }
-            return false;
-        }
-
-        public async Task<bool> DeleteAnimal(Animal animal)
-        {
-            var result = await AnimalCollection.DeleteOneAsync(x => x.Id == animal.Id);
-            if (result.IsAcknowledged && result.DeletedCount > 0)
-            {
-                var cdbh = Factory.GetCustomerDbHelper();
-                await cdbh.RemoveAnimalFromCustomer(animal);
-                return true;
-            }
-            return false;
-        }
-
-        public async Task<bool> UpdateCustomer(Customer owner)
-        {
-            owner.FirstName = Prettify(owner.FirstName);
-            owner.LastName = Prettify(owner.LastName);
-            var filter = Builders<Customer>.Filter.Eq("Id", owner.Id);
-            var result = await CustomerCollection.ReplaceOneAsync(filter, owner, new ReplaceOptions { IsUpsert = true });
-            return result.IsModifiedCountAvailable && result.ModifiedCount > 0;
-        }
-
-        public async Task<bool> DeleteCustomerById(string id)
-        {
-            var cdbh = Factory.GetCustomerDbHelper();
-            await cdbh.DeleteAnimalsTogetherWithCustomer(id);
-            var result = await CustomerCollection.DeleteOneAsync(x => x.Id == id);
-            return result.IsAcknowledged && result.DeletedCount > 0;
         }
     }
 }
